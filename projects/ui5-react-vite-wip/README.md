f2efd1d9146c07f2a1686b505b95356eebd73c4f
Jan-André Zinser <zinserjan@gmail.com>
2024-01-05 10:04:42 +0100

    chore: clean up

--- projects/ui5-react-vite-wip/README.md
+++
@@ -1,1 +0,0 @@
-# UI + React + Vite
--- projects/ui5-react-vite-wip/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./main";
-
-import { RenderOptions, render } from "virtual:ui5-register-app";
-
-const options: RenderOptions = {
-  resolveUri: (path) => path,
-};
-
-render(document.getElementById("root")!, options);
