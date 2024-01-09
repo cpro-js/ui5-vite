c5e852507024ef00b18b5213ab4b4946895c6686
Jan-André Zinser <zinserjan@gmail.com>
2024-01-09 17:15:53 +0100

    feat: add more functionalities

--- projects/ui5-react-vite/ui5/Component.ts
+++ projects/ui5-react-vite/ui5/Component.ts
@@ -1,13 +1,17 @@
 import type MessageBox from "sap/m/MessageBox";
+import type UI5Event from "sap/ui/base/Event";
+import EventBus from "sap/ui/core/EventBus";
 import RenderManager from "sap/ui/core/RenderManager";
+import HashChanger from "sap/ui/core/routing/HashChanger";
 import UIComponent from "sap/ui/core/UIComponent";
 import Device from "sap/ui/Device";
-import { render } from "virtual:@cpro-js/ui5-vite-app-plugin/runtime";
+import { render, RenderOptions } from "virtual:@cpro-js/ui5-vite-app-plugin/runtime";
 
 /**
  * @namespace react.ui5.vite
  */
 export default class Component extends UIComponent {
+  private eventBus!: EventBus;
   private scriptPromise!: Promise<void>;
   private appElementId!: string;
   private unmountApp?: () => void;
@@ -15,14 +19,15 @@ export default class Component extends UIComponent {
   public static metadata = {
     manifest: "json",
   };
-
   /**
    * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-   * @public
-   * @override
    */
-  public init(): void {
-    super.init();
+  constructor() {
+    super();
+    this.eventBus = new EventBus();
+    sap.ui.getCore().attachThemeChanged(this._onThemeChanged, this);
+    sap.ui.getCore().attachLocalizationChanged(this._onLocalizationChanged, this);
+
     // load custom App code
     this.scriptPromise = this._loadScriptModule(this.resolveUri("/main.tsx"));
     this.appElementId = this.createId("custom-app");
@@ -56,9 +61,7 @@ export default class Component extends UIComponent {
       const rtl = sap.ui.getCore().getConfiguration().getRTL();
       el.dir = rtl ? "rtl" : "ltr";
 
-      this.unmountApp = render(el, {
-        component: this,
-      });
+      this.unmountApp = render(el, this.getRenderOptions());
     } catch (e: Error | unknown) {
       console.error(e);
       // lazy load to speed up initial page load
@@ -70,12 +73,90 @@ export default class Component extends UIComponent {
   }
 
   public destroy() {
+    console.log("destroy");
     this.unmountApp?.();
+    this.eventBus.destroy();
+    sap.ui.getCore().detachThemeChanged(this._onThemeChanged, this);
+    sap.ui.getCore().detachLocalizationChanged(this._onLocalizationChanged, this);
+
     super.destroy();
+  }
+
+  public getRenderOptions(): RenderOptions {
+    return {
+      component: this,
+    };
   }
 
   public isLaunchpad(): boolean {
     return sap.ushell != null;
+  }
+
+  public getNavigationContext(): { semanticObject?: string; action?: string } {
+    return this._parseShellHash(new HashChanger().getHash());
+  }
+
+  public setRelatedApps(relatedApps: Array<{ title: string; subtitle?: string; icon?: string; intent: string }>) {
+    return this.getService("ShellUIService").then(function (service: sap.ushell.ui5service.ShellUIService) {
+      service.setRelatedApps(relatedApps);
+    });
+  }
+
+  /**
+   * Displays the given title in the shell header. This method should not be called if the app calling the
+   * method is not currently displayed in the Fiori Launchpad.
+   *
+   * @param title
+   */
+  public setShellTitle(title: string) {
+    if (!this.isLaunchpad()) {
+      return;
+    }
+
+    return this.getService("ShellUIService").then(function (service: sap.ushell.ui5service.ShellUIService) {
+      service.setTitle(title);
+    });
+  }
+
+  public setBackNavigation(cb: () => void) {
+    if (!this.isLaunchpad()) {
+      return;
+    }
+
+    return this.getService("ShellUIService").then(function (service: sap.ushell.ui5service.ShellUIService) {
+      // @ts-ignore: incomplete types
+      service.setBackNavigation(function () {
+        cb();
+      });
+    });
+  }
+
+  /**
+   * Get the current locale
+   * BCP-47 language list, e.g. de-DE, en-US, en
+   */
+  public getLocale(): string {
+    return sap.ui.getCore().getConfiguration().getLanguage();
+  }
+
+  /**
+   * Get the current theme, e.g. sap_fiori_3
+   */
+  public getTheme(): string {
+    return sap.ui.getCore().getConfiguration().getTheme();
+  }
+
+  subscribeToThemeChanges(cb: (theme: string) => void) {
+    this.eventBus.subscribe("onLocaleChanged", function (_channel, _eventId, data) {
+      cb((data as { locale: string }).locale);
+    });
+  }
+
+  /**
+   * Get current animation mode, e.g. basic, full, minimal or none
+   */
+  public getAnimationMode(): string {
+    return sap.ui.getCore().getConfiguration().getAnimationMode();
   }
 
   /**
@@ -129,6 +210,41 @@ export default class Component extends UIComponent {
     }
     // nothing found
     return;
+  }
+
+  private _onThemeChanged(event: UI5Event<{ theme: string }>) {
+    this.eventBus.publish("onThemeChanged", {
+      theme: event.getParameter("theme"),
+    });
+  }
+
+  private _onLocalizationChanged(event: UI5Event<{ changes: { language?: string } }>) {
+    const { language } = event.getParameter("changes");
+    if (language != null) {
+      this.eventBus.publish("onLocaleChanged", {
+        locale: language,
+      });
+    }
+  }
+
+  private _parseShellHash(sHash: string): { semanticObject?: string; action?: string } {
+    const SPLIT_SHELL_APP_HASH = /^(?:#|)([\S\s]*?)(&\/[\S\s]*)?$/;
+    const EXTRACT_SHELL_HASH = /^(([A-Za-z0-9_/]+)-([A-Za-z0-9_/-]+)(~([A-Z0-9a-z=+/]+))?)?([?]([^&]|(&[^/]))*&?)?$/;
+    const splitResult = SPLIT_SHELL_APP_HASH.exec(sHash);
+
+    if (splitResult == null || splitResult[1] === "" || !EXTRACT_SHELL_HASH.test(splitResult[1])) {
+      return {
+        semanticObject: undefined,
+        action: undefined,
+      };
+    }
+
+    const extractResult = EXTRACT_SHELL_HASH.exec(splitResult[1]);
+
+    return {
+      semanticObject: extractResult?.[2],
+      action: extractResult?.[3],
+    };
   }
 
   private _loadScriptModule(srcUrl: string): Promise<void> {
