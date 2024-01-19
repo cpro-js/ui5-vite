c62e556fa1b39034ee0e9f1d34d4e16f02798da1
Jan-André Zinser <zinserjan@gmail.com>
2024-01-19 17:08:06 +0100

    fix: ESM/CJS config

--- packages/ui5-vite-app-plugin/vite.config.ts
+++ packages/ui5-vite-app-plugin/vite.config.ts
@@ -4,10 +4,11 @@ import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    emptyOutDir: true,
     ssr: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
+      formats: ["es", "cjs"],
     },
     sourcemap: "inline",
     minify: false,
